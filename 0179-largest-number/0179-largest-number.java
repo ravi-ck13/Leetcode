3c237dd21a78d6108b0744fdc2744d23f1a4f6e3
Ravi Kumar <143173748+ravi-ck13@users.noreply.github.com>
2024-08-17 10:48:26 +0530

    Time: 11 ms (10.92%), Space: 44.7 MB (6.65%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -7,7 +7,19 @@ class Solution
         {
             numstr[i]=String.valueOf(nums[i]);
         }    
-        Arrays.sort(numstr,(a,b)->(b+a).compareTo(a+b));
+// Arrays.sort(numstr,(a,b)->(b+a).compareTo(a+b));        
+        for(int i=0;i<nums.length-1;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+               if ((numstr[i] + numstr[j]).compareTo(numstr[j] + numstr[i]) < 0)
+                {
+                    String temp=numstr[i];
+                    numstr[i]=numstr[j];
+                    numstr[j]=temp;
+                }
+            }
+        }
         if(numstr[0].equals("0"))
         {
             return "0";
